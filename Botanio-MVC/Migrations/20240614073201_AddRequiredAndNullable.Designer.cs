5c9c7ed573488c93bdfd90cafdcef7be65b39656
Lukas <lovretic2002@gmail.com>
2024-06-14 09:33:15 +0200

    migracija

--- Botanio-MVC/Migrations/20240614073201_AddRequiredAndNullable.Designer.cs
+++ Botanio-MVC/Migrations/20240614073201_AddRequiredAndNullable.Designer.cs
@@ -3,6 +3,7 @@ using Botanio_MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Botanio_MVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240614073201_AddRequiredAndNullable")]
+    partial class AddRequiredAndNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Botanio-MVC/Migrations/20240614073201_AddRequiredAndNullable.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Botanio_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRequiredAndNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Species",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SoilType",
+                table: "Habitats",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Plants",
+                keyColumn: "PlantId",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "fiddle-leaf-fig.webp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Species",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SoilType",
+                table: "Habitats",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Plants",
+                keyColumn: "PlantId",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "fiddle-leaf-fig");
+        }
+    }
+}
